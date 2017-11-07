361a108d70569310eb60c64b8bda323009e524a1
Natalie Ung <ung.natalie@gmail.com>
2017-11-07 12:23:27 -0500

    completed rendering

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
+const nunjucks = require('nunjucks'); 
+const people = [{name: 'Full'}, {name:'Stacker'}, {name: 'Son'}];
 // const bodyParser = require('body-parser');
+
+
+app.engine('html', nunjucks.render);
+app.set('view engine', 'html');
+nunjucks.configure('views', {noCache: true});
+
+// res.render('index', {title: 'Hall of Fame', people: people});
+
 
 app.use(function (req, res, next) {
 	console.log(req.method + req.path);
@@ -12,13 +22,12 @@ app.use(function (req, res, next) {
 
 
 app.get('/', function (req, res, next) {
-	res.send('Welcome!');
-	next();
+	res.render('index', {title: 'Hall of Fame', people: people});
 });
+
 
 app.get('/news', function (req, res, next) {
 	res.send('Welcome to the not news page!');
-	next();
 });
 
 
@@ -28,4 +37,17 @@ app.listen(3000, function() {
 	console.log('Server listening');
 });
 
-//Oczane was here!
+
+
+
+
+
+
+
+
+
+// nunjucks.configure('views', {noCache: true});
+// nunjucks.render('index.html', locals, function (err, output) {
+// 	console.log(output);
+// });
+
