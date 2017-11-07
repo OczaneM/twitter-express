b86444f92badaaea23319fa6415a10aba27576eb
Natalie Ung <ung.natalie@gmail.com>
2017-11-07 10:55:51 -0500

    configuring express

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const app = express();
+// const bodyParser = require('body-parser');
+
+app.use(function (req, res, next) {
+	console.log(req.method + req.path);
+	next();
+});
 
 
+// app.post()
 
 
+app.get('/', function (req, res, next) {
+	res.send('Welcome!');
+	next();
+});
+
+app.get('/news', function (req, res, next) {
+	res.send('Welcome to the not news page!');
+	next();
+});
 
 
 
